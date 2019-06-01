ff4085f9962c0b4770ae16e0138c4b58d650ddf6
Card007 <izd@qq.com>
2019-06-01 12:16:44 +0800

    update code

--- proxy_pool.js
+++ proxy_pool.js
@@ -5,7 +5,7 @@ var sqlite3 = require('sqlite3')
 //实现爬取分析功能
 //接下来是数据库和去除不能用的ip
 
-var db = new sqlite3.Database('Proxy.db', (err) => {
+const db = new sqlite3.Database('Proxy.db', (err) => {
     if(!err){
         console.log('打开成功')
     } else {
@@ -15,24 +15,24 @@ var db = new sqlite3.Database('Proxy.db', (err) => {
 
 db.run('CREATE TABLE proxy(ip char(15), port char(15), type char(15))',(err) => {})
 
-var useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
+const useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36'
 
-var headers = {
+const headers = {
     'User-Agent': useragent,
 }
 
 //添加数据文件
-var insertDb = function(ip, port, type){
+const insertDb = function(ip, port, type){
     db.run("INSERT INTO proxy VALUES(?, ?, ?)",[ip,port,type])
 }
 
 //提取优化文件数据
-var clearN = function(l){
-    var index = 0
+const clearN = function(l){
+    let index = 0
     for (let i = 0; i < l.length; i++) {
         if(l[i] === '' || l[i] === '\n'){
         }else{
-            var ips = l[i].replace('\n','')
+            let ips = l[i].replace('\n','')
             if (index === 0){
                 var ip = ips
                 console.log('爬取ip:' + ip)
@@ -48,9 +48,9 @@ var clearN = function(l){
 }
 
 //分析网页内容
-var loadHtml = function(data){
-    var l = []
-    var e = cheerio.load(data)
+const loadHtml = function(data){
+    let l = []
+    let e = cheerio.load(data)
     e('tr').each(function(i, elem){
         l[i] = e(this).text()
     })
@@ -60,7 +60,7 @@ var loadHtml = function(data){
 }
 
 //链接网络
-var requestProxy = function(options){
+const requestProxy = function(options){
     return new Promise((resolve, reject) => {
         request(options, function(err, response, body){
             if(err === null && response.statusCode === 200){
@@ -75,15 +75,15 @@ var requestProxy = function(options){
 }
 
 //生成网址
-var ipUrl = function(resolve){
-    var url = 'http://www.xicidaili.com/nn/'
+const ipUrl = function(resolve){
+    const url = 'http://www.xicidaili.com/nn/'
 
-    var options = {
+    let options = {
         url:'http://www.xicidaili.com/nn/',
         headers,
     }
 
-    var arr = []
+    let arr = []
    
     for (let i = 1; i <= 5; i++) {
         options.url = url + i
@@ -95,25 +95,25 @@ var ipUrl = function(resolve){
 }
 
 //从数据库提取所有ip
-var allIp = function(callback){
+const allIp = function(callback){
     return db.all('select * from proxy', callback)
 }
 
 //代理ip对象
-var Proxys = function(ip,port,type){
+const Proxys = function(ip,port,type){
     this.ip = ip
     this.port = port
     this.type = type
 }
 
 //提取所有ip，通过check函数检查
-var runIp = function(resolve){
-    var arr = []
+const runIp = function(resolve){
+    let arr = []
 
     allIp((err,response) => {
         for (let i = 0; i < response.length; i++) {
-            var ip = response[i]
-            var proxy = new Proxys(ip.ip, ip.port, ip.type)
+            let ip = response[i]
+            let proxy = new Proxys(ip.ip, ip.port, ip.type)
             arr.push(check(proxy, headers))
         }
         Promise.all(arr).then(function(){
@@ -126,7 +126,7 @@ var runIp = function(resolve){
 }
 
 //检测ip
-var check = function(proxy, headers){
+const check = function(proxy, headers){
     return new Promise((resolve, reject) => {
         request({
             url:'http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js',
@@ -149,7 +149,7 @@ var check = function(proxy, headers){
 }
 
 //删除命令
-var removeIp = function(ip){
+const removeIp = function(ip){
     db.run(`DELETE FROM proxy WHERE ip = '${ ip }'`, function(err){
         if(err){
             console.log(err)
